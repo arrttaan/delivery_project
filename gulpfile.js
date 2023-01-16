45b2c6986d2a8f1cb68e26627d04164904d198e9
arrttaan <artan1999@live.com>
2023-01-16 16:43:23 +0100

    uploading

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function compileSass(done) {
+    gulp.src('./scss/main.scss')
+        .pipe(sass().on('error' , sass.logError))
+        .pipe(gulp.dest('./css'));
+    done();
+}
+
+exports.compileSass = compileSass;
+exports.default = gulp.parallel(compileSass);
